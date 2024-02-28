45371eda42de0cc33b462cf353ce248fb771a143
TafsirHarunTurjo <th.turja94@gmail.com>
2024-02-28 15:42:52 +0600

    datatypes

--- tut2.php
+++ tut2.php
@@ -7,12 +7,36 @@
 </head>
 <body>
     <?php
-        function cube($num){
-           return $num*$num*$num;
-        }
-        $cubeResult = cube(5);
-        echo $cubeResult;
+       function processmakrs($marksarr){
+        $sum =0;
+        foreach($marksarr as $value){
+            $sum += $value;
 
+        }
+        return $sum;
+        
+        
+       }
+
+    $tjmarks = [12,33,81,29];
+    $summarks1 = processmakrs($tjmarks);
+
+    $mirmarks = [22,33,44,66];
+    $summarks2 = processmakrs($mirmarks);
+
+    $pmarks = [81,24,21,66];
+    $summarks3 = processmakrs($pmarks);
+
+    $laramarks = [ 92,18.,91,18];
+    $summarks4 = processmakrs($laramarks);
+   
+    
+    
+    
+    echo "Turjo Got $summarks1\n<br>";
+    echo "Mirza Got $summarks2\n<br>";
+    echo "p Got $summarks3\n<br>";
+    echo "lara Got $summarks4\n<br>";
     ?>
 </body>
 </html>
